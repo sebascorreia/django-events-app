dd87c044e3f412c1ef69be651443660b602d949f
sebascorreia <sebascorreia@gmail.com>
2025-05-06 14:28:43 +0100

    initial custom user model

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,6 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -31,6 +30,7 @@ DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = []
 
 
+AUTH_USER_MODEL = 'eventsapp.CustomUser'
 # Application definition
 
 INSTALLED_APPS = [
