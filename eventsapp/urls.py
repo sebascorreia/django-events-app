21d3ff71744d921a0bc0e5ac1cd7c01c86c52fcc
sebascorreia <sebascorreia@gmail.com>
2025-05-15 15:29:21 +0100

    Added Home page and maps to every page.

--- eventsapp/urls.py
+++ eventsapp/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.HomeView.as_view(), name="home"),
     path("artist/<int:pk>/", views.ArtistDetailView.as_view(), name="artist_detail"),
     path("venue/<int:pk>/", views.VenueDetailView.as_view(), name="venue_detail"),
     path("event/<int:pk>/", views.EventDetailView.as_view(), name="event_detail"),
--- eventsapp/views.py
+++ eventsapp/views.py
@@ -9,8 +9,29 @@ import json
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'eventsapp/home.html')
+class HomeView(generic.TemplateView):
+    template_name = 'eventsapp/home.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['events']= Event.objects.all()
+        context['artists'] = Artist.objects.all()
+        context['venues'] = Venue.objects.all()
+
+        markers = []
+        for event in Event.objects.all():
+            markers.append({
+                "lat": event.venue.location.y,
+                "lng": event.venue.location.x,
+                "title": escape(event.name),
+                "venue": escape(event.venue.name),
+                "date": str(event.date),
+                "price": float(event.ticket_price)
+
+            })
+        context['GOOGLE_MAPS_API_KEY'] = settings.GOOGLE_MAPS_API_KEY    
+        context['events_markers_json'] = json.dumps(markers)
+        return context
+    
 class ArtistDetailView(generic.DetailView):
     model = Artist
     template_name = 'eventsapp/artist.html'
@@ -28,6 +49,7 @@ class ArtistDetailView(generic.DetailView):
                 "title": escape(event.name),
                 "venue": escape(event.venue.name),
                 "date": str(event.date),
+                "price": float(event.ticket_price)
             })
         context['GOOGLE_MAPS_API_KEY'] = settings.GOOGLE_MAPS_API_KEY
         context['events_markers_json'] = json.dumps(markers)
@@ -45,4 +67,8 @@ class VenueDetailView(generic.DetailView):
     model = Venue
     template_name = 'eventsapp/venue.html'
     context_object_name = 'venue'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['GOOGLE_MAPS_API_KEY'] = settings.GOOGLE_MAPS_API_KEY
+        return context
 
